49ec405d691398ee6d74c6e595505680a5897bb7
Luke Harris <8006624+Lukeharris30@users.noreply.github.com>
2023-04-20 14:33:03 -0600

    chore: format

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,73 +1,55 @@
-  
 <script setup>
 import { useUser } from '../stores/user.js'
-
-const  u  = useUser()
-
-
+const u = useUser()
 </script>
 
 <template>
-    <q-layout view="hHh lpR fFf">
-  
-      <q-header  class="bg-dark text-white" height-hint="98">
-        <q-toolbar class="q-pt-lg">
-          <!-- <q-btn dense flat  icon="menu" @click="toggleLeftDrawer" /> -->
-  
-          <q-toolbar-title>
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-            </q-avatar>
-           Supplier Portal
-          </q-toolbar-title>
-  
-          <!-- <q-btn dense flat  icon="menu" @click="toggleRightDrawer" /> -->
-           
-        <q-item v-if="u.user"  >
+  <q-layout view="hHh lpR fFf">
+    <q-header class="bg-dark text-white" height-hint="98">
+      <q-toolbar class="q-pt-lg">
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          Supplier Portal
+        </q-toolbar-title>
+        <q-item v-if="u.user">
           <q-item-section side>
             <q-avatar square size="40px">
               <img :src="u.user.imageUrl" />
             </q-avatar>
           </q-item-section>
-          
           <q-item-section>
-            <q-item-label>{{u.user.name}}</q-item-label>
-            <q-badge v-if="u.user.isAdmin"  color="primary">srammie</q-badge>
+            <q-item-label>{{ u.user.name }}</q-item-label>
+            <q-badge v-if="u.user.isAdmin" color="primary">srammie</q-badge>
           </q-item-section>
-          
-
         </q-item>
-        </q-toolbar> 
-  
-        <q-tabs align="left">
-          
-          <q-route-tab to="/home" label="home" />
-          <q-route-tab to="/upload" label="upload" />
-          <q-route-tab to="/" label="login" />
-        </q-tabs>
-       
-        
-      </q-header>
-  
-      <q-page-container>
-        <!-- <router-view /> -->
-        <q-page padding>
-            <slot></slot>
-        </q-page>
-        
-      </q-page-container>
-  
-      <q-footer class="q-dark text-white">
-        <q-toolbar>
-          <q-toolbar-title>
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-            </q-avatar>
-            <div>Supplier Portal</div>
-          </q-toolbar-title>
-        </q-toolbar>
-      </q-footer>
-  
-    </q-layout>
-  </template>
+      </q-toolbar>
+      <q-tabs align="left">
+        <q-route-tab to="/home" label="home" />
+        <q-route-tab to="/upload" label="upload" />
+        <q-route-tab to="/access" label="User Access" />
+        <q-route-tab to="/" label="login" />
+      </q-tabs>
+    </q-header>
+
+    <q-page-container>
+      <q-page padding>
+        <slot></slot>
+      </q-page>
+    </q-page-container>
+
+    <q-footer class="q-dark text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          <div>Supplier Portal</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
 
