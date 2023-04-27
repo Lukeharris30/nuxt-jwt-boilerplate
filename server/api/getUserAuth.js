70f4f73e34cf0b109def1db2770ee3ea75caa620
Luke Harris <8006624+Lukeharris30@users.noreply.github.com>
2023-04-27 11:53:35 -0600

    feat: add try catch

--- server/api/getUserAuth.js
+++ server/api/getUserAuth.js
@@ -1,17 +1,25 @@
-import {OAuth2Client} from 'google-auth-library'
+import { OAuth2Client } from 'google-auth-library';
 
-export default defineEventHandler( async (event) => {
-    const body = await readBody(event)
-//   verify token
-    const client = new OAuth2Client()
-    async function verify() {
-        const ticket = await client.verifyIdToken({
-            idToken: body,
-            audence: process.env.NUXT_GOOGLE_SECRET
-        })
-        const payload = ticket.getPayload()
-        return payload
-    }
-    
-  return  verify().catch(console.error)
-  })
+// Verify token function
+async function verifyToken(idToken) {
+  const client = new OAuth2Client();
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken,
+      audience: process.env.NUXT_GOOGLE_SECRET,
+    });
+    const payload = ticket.getPayload();
+    return payload;
+  } catch (error) {
+    console.error('Error while verifying token:', error);
+    throw error;
+  }
+}
+
+// Event handler
+export default defineEventHandler(async (event) => {
+  const idToken = await readBody(event);
+
+  // Verify token and return the user
+  return verifyToken(idToken);
+});
